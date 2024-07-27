187afa09d5c30b624b89b2400f3c29101f644ffb
AidenYav <yavaiden@gmail.com>
2024-07-26 19:01:02 -0700

    Pinch Zoom Test

--- EventHandler.js
+++ EventHandler.js
@@ -116,8 +116,13 @@ let lastTapTime = 0;
 let lastRotateTime = 0;
 
 // listen to events...
-hammer.on("panleft panright panup pandown tap press", function(ev) {
+hammer.get('pinch').set({ enable: true });
+hammer.on("panleft panright panup pandown tap press pinch", function(ev) {
     //console.log(ev.type +" gesture detected.");
+    if (ev.type == "pinch"){
+        console.log('Zoom scale: ' + ev.scale);
+        base.scale.add(ev.scale * 0.01);
+    }
 
     if (ev.type == "tap"){
         const currentTime = Date.now();
